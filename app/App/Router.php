662fb3a0260911813c86686e1d572b287a37bc7d
rudigigih1907 <rudigigih0719@gmail.com>
2023-01-18 10:33:26 +0700

    Memilih Controller

--- app/App/Router.php
+++ app/App/Router.php
@@ -17,6 +17,20 @@ class Router
 
     public static function run():void
     {
-        
+        $path = "/";
+        if (isset($_SERVER['PATH_INFO'])) {
+            $path = $_SERVER['PATH_INFO'];
+        }
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        foreach (self::$routes as $route) {
+            if ($path == $route['path'] && $method == $route['method']) {
+                echo "Controller : " . $route['controller'] . ', Function : ' . $route['function'];
+                return;
+            }
+        }
+        http_response_code(404);
+        echo 'CONTROLLER NOT FOUND';
     }
 }
